231fb49f5716999fbcf74091a4c5279a1a667221
strmfelix <email@felixstrohmeier.com>
2024-12-02 21:25:42 +0100

    feat(ui): enhance login page design and workspace layout

--- app/[domain]/login/page.tsx
+++ app/[domain]/login/page.tsx
@@ -3,10 +3,13 @@ import Bckgrnd from '@/components/icons/bckgrnd'
 
 export default function Page() {
   return (
-    <div className='flex size-full items-center justify-center bg-muted'>
-      <div className='space-y-4 rounded-2xl border bg-background p-9'>
-        <Bckgrnd className='size-9' />
+    <div className='relative flex size-full items-center justify-center'>
+      <div className='absolute inset-0 -z-40 size-full bg-[url(https://bipjaqofywtmfirkmtdw.supabase.co/storage/v1/object/public/web/bg.webp)] bg-cover bg-center' />
+      <div className='absolute -z-30 size-96 bg-gradient-to-t from-background to-[#B79891] blur-[100px]' />
+      <div className='absolute inset-0 bottom-0 -z-40 w-full bg-gradient-to-t from-background to-transparent' />
+      <div className='relative z-20 space-y-4 rounded-2xl border bg-background p-9'>
         <div>
+          <Bckgrnd className='mb-6 size-9' />
           <h4>Welcome back!</h4>
           <p className='text-sm text-muted-foreground'>
             Log in to your account to continue.
